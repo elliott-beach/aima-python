99f0569531673349d776d97797e14b24cecfca7a
Elliott Beach <elliott2.71828@gmail.com>
2017-10-29 22:23:37 -0500

    CSCI 4511W: Find the starcraft 2 junky

--- csp.py
+++ csp.py
@@ -654,23 +654,23 @@ class Sudoku(CSP):
 
 def Zebra():
     """Return an instance of the Zebra Puzzle."""
-    Colors = 'Red Yellow Blue Green Ivory'.split()
-    Pets = 'Dog Fox Snails Horse Zebra'.split()
-    Drinks = 'OJ Tea Coffee Milk Water'.split()
-    Countries = 'Englishman Spaniard Norwegian Ukranian Japanese'.split()
-    Smokes = 'Kools Chesterfields Winston LuckyStrike Parliaments'.split()
-    variables = Colors + Pets + Drinks + Countries + Smokes
+    # Colors = 'Red Yellow Blue Green Ivory'.split()
+    # Pets = 'Dog Fox Snails Horse Zebra'.split()
+    # Drinks = 'OJ Tea Coffee Milk Water'.split()
+    # Names = 'Englishman Spaniard Norwegian Ukranian Japanese'.split()
+    # Smokes = 'Kools Chesterfields Winston LuckyStrike Parliaments'.split()
+    Names = 'Albert Dietfried Virgiljus Bricius Gallchobhar'.split()
+    Sports = 'rugby baseball debate soccer starcraft2'.split()
+    Lawns = 'grass astroturf trees rocks flowers'.split()
+    Transport = 'walk bicycle motorcycle bus car'.split()
+    variables = Names + Sports + Lawns + Transport
     domains = {}
     for var in variables:
         domains[var] = list(range(1, 6))
-    domains['Norwegian'] = [1]
-    domains['Milk'] = [3]
-    neighbors = parse_neighbors("""Englishman: Red;
-                Spaniard: Dog; Kools: Yellow; Chesterfields: Fox;
-                Norwegian: Blue; Winston: Snails; LuckyStrike: OJ;
-                Ukranian: Tea; Japanese: Parliaments; Kools: Horse;
-                Coffee: Green; Green: Ivory""", variables)
-    for type in [Colors, Pets, Drinks, Countries, Smokes]:
+    domains['debate'] = [2]
+    domains['bicycle'] = [4]
+    neighbors = parse_neighbors("""Albert: baseball; Dietfried: grass; Virgiljus: rugby; Gallchobhar: walk; Bricius: Virgiljus; astroturf: baseball; flowers: trees rocks; baseball: motorcycle; soccer: bus; trees: car; Gallchobhar: rugby""", variables)
+    for type in [Names, Sports, Lawns, Transport]:
         for A in type:
             for B in type:
                 if A != B:
@@ -682,37 +682,35 @@ def Zebra():
     def zebra_constraint(A, a, B, b, recurse=0):
         same = (a == b)
         next_to = abs(a - b) == 1
-        if A == 'Englishman' and B == 'Red':
+        right_of = a - b == 1
+        if A == 'Albert' and B == 'baseball':
             return same
-        if A == 'Spaniard' and B == 'Dog':
+        if A == 'Dietfried' and B == 'grass':
             return same
-        if A == 'Chesterfields' and B == 'Fox':
+        if A == 'Virgiljus' and B == 'rugby':
+            return same
+        if A == 'Gallchobhar' and B == 'walk':
+            return same
+        if A == 'Bricius' and B == 'Virgiljus':
             return next_to
-        if A == 'Norwegian' and B == 'Blue':
+        if A == 'astroturf' and B == 'baseball':
+            return same
+        if A == 'flowers' and B in ['trees', 'rocks']:
             return next_to
-        if A == 'Kools' and B == 'Yellow':
+        if A == 'baseball' and B == 'motorcycle':
             return same
-        if A == 'Winston' and B == 'Snails':
+        if A == 'soccer' and B == 'bus':
             return same
-        if A == 'LuckyStrike' and B == 'OJ':
-            return same
-        if A == 'Ukranian' and B == 'Tea':
-            return same
-        if A == 'Japanese' and B == 'Parliaments':
-            return same
-        if A == 'Kools' and B == 'Horse':
-            return next_to
-        if A == 'Coffee' and B == 'Green':
-            return same
-        if A == 'Green' and B == 'Ivory':
-            return a - 1 == b
+        if A == 'trees' and B == 'car':
+            return right_of
+        if A == 'Gallchobhar' and B == 'rugby':
+            return right_of
         if recurse == 0:
             return zebra_constraint(B, b, A, a, 1)
-        if ((A in Colors and B in Colors) or
-                (A in Pets and B in Pets) or
-                (A in Drinks and B in Drinks) or
-                (A in Countries and B in Countries) or
-                (A in Smokes and B in Smokes)):
+        if ((A in Transport and B in Transport) or
+                (A in Sports and B in Sports) or
+                (A in Names and B in Names) or
+                (A in Lawns and B in Lawns)):
             return not same
         raise Exception('error')
     return CSP(variables, domains, neighbors, zebra_constraint)
